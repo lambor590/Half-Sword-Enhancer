a93711db19745fbf8a139ddfb9498c8157ddea29
lambor590 <lambor590@gmail.com>
2025-01-26 00:04:20 +0100

    g

--- Mod/gui/Gui.cpp
+++ Mod/gui/Gui.cpp
@@ -7,10 +7,19 @@ bool Gui::isVisible = true;
 Logger logger("Gui");
 
 LRESULT CALLBACK Gui::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    auto& instance = *s_instance;
+
     if (GetAsyncKeyState(VK_INSERT) & 1)
         isVisible = !isVisible;
 
-    if (&isVisible && ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+    // Capturar teclas para keybindings
+    if (instance.waitingForKey && msg == WM_KEYDOWN) {
+        instance.waitingForKey = false;
+        ActionManager::Get().SetKeyBinding(instance.pendingKeybindActionId, (int)wParam);
+        return true;
+    }
+
+    if (isVisible && ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
         return true;
 
     return CallWindowProc(originalWndProc, hWnd, msg, wParam, lParam);
@@ -28,8 +37,87 @@ void Gui::Setup() {
     io.IniFilename = "HS-Enhancer-imgui.ini";
 
     ImGui::StyleColorsDark();
+    RegisterActions();
 
     s_instance = this;
+}
+
+void Gui::RegisterActions() {
+    auto& actions = ActionManager::Get();
+
+    // Combat actions
+    actions.RegisterAction("combat.autoblock", {
+        "Auto Block",
+        false,
+        VK_F1,
+        []() { /* Implementar auto block */ }
+    });
+
+    actions.RegisterAction("combat.parry", {
+        "Auto Parry",
+        false,
+        VK_F2,
+        []() { /* Implementar auto parry */ }
+    });
+
+    // Movement actions
+    actions.RegisterAction("movement.speed", {
+        "Speed Control",
+        false,
+        VK_F3,
+        []() { /* Implementar speed control */ }
+    });
+}
+
+void Gui::RenderActionToggle(const std::string& id) {
+    if (Action* action = ActionManager::Get().GetAction(id)) {
+        ImGui::Checkbox(action->name.c_str(), &action->enabled);
+    }
+}
+
+void Gui::RenderKeybind(const std::string& id) {
+    if (Action* action = ActionManager::Get().GetAction(id)) {
+        ImGui::SameLine();
+        char keyName[32];
+        sprintf_s(keyName, "[%c]", action->keyBinding ? action->keyBinding : '?');
+
+        if (ImGui::Button(keyName)) {
+            waitingForKey = true;
+            pendingKeybindActionId = id;
+        }
+    }
+}
+
+void Gui::RenderGameplayTab() {
+    if (ImGui::CollapsingHeader("Combat")) {
+        ImGui::Indent();
+        RenderActionToggle("combat.autoblock");
+        RenderKeybind("combat.autoblock");
+        
+        RenderActionToggle("combat.parry");
+        RenderKeybind("combat.parry");
+        ImGui::Unindent();
+    }
+
+    if (ImGui::CollapsingHeader("Movement")) {
+        ImGui::Indent();
+        RenderActionToggle("movement.speed");
+        RenderKeybind("movement.speed");
+        ImGui::Unindent();
+    }
+}
+
+void Gui::RenderVisualTab() {
+    // TODO: Implementar visual tab
+}
+
+void Gui::RenderDebugTab() {
+    ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+    ImGui::Text("Delta Time: %.3f ms", 1000.0f / ImGui::GetIO().Framerate);
+}
+
+void Gui::RenderSettingsTab() {
+    // TODO: Implementar settings tab
 }
 
 void Gui::Render() {
@@ -44,9 +132,24 @@ void Gui::Render() {
         ImGuiWindowFlags_MenuBar |
         ImGuiWindowFlags_NoCollapse);
 
-    if (ImGui::CollapsingHeader("Debug")) {
-        ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
-        ImGui::Text("Delta Time: %.3f ms", 1000.0f / ImGui::GetIO().Framerate);
+    if (ImGui::BeginTabBar("MainTabBar")) {
+        if (ImGui::BeginTabItem("Gameplay")) {
+            RenderGameplayTab();
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Visual")) {
+            RenderVisualTab();
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Debug")) {
+            RenderDebugTab();
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Settings")) {
+            RenderSettingsTab();
+            ImGui::EndTabItem();
+        }
+        ImGui::EndTabBar();
     }
 
     ImGui::End();
--- Mod/gui/Gui.h
+++ Mod/gui/Gui.h
@@ -8,13 +8,13 @@
 #include "backends/imgui_impl_dx11.h"
 #include "backends/imgui_impl_win32.h"
 #include "Logger.h"
+#include "core/Action.h"
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class Gui : public IRenderCallback {
 public:
     Gui() = default;
-
     Gui(const Gui&) = delete;
     Gui& operator=(const Gui&) = delete;
 
@@ -23,7 +23,18 @@ public:
     static LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 private:
+    void RegisterActions();
+    void RenderGameplayTab();
+    void RenderVisualTab();
+    void RenderDebugTab();
+    void RenderSettingsTab();
+    
+    void RenderActionToggle(const std::string& id);
+    void RenderKeybind(const std::string& id);
+    
     static Gui* s_instance;
     static WNDPROC originalWndProc;
     static bool isVisible;
+    bool waitingForKey = false;
+    std::string pendingKeybindActionId;
 };
