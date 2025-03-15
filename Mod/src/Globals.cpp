211ba58725d14fcf256ca8ecc166d42b3fb4ebe0
lambor590 <lambor590@gmail.com>
2025-03-15 17:51:18 +0100

    feat: add DirectXHook global reference
    - Include DirectXHook in Globals.cpp
    - Initialize g_DirectXHook as a global pointer

--- Mod/src/Globals.cpp
+++ Mod/src/Globals.cpp
@@ -1,6 +1,8 @@
 #include "GlobalDefinitions.h"
 #include "Hooks/GameHook.h"
+#include "Hooks/DirectXHook.h"
 #include "ConfigManager.h"
 
 GameHook* g_GameHook = &GameHook::Get();
 ConfigManager& g_ConfigManager = ConfigManager::Get();
+DirectXHook* g_DirectXHook = nullptr;
