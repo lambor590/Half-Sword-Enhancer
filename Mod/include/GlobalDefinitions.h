70ecc3679db09b8ee681fd2b6710a9083a0494af
lambor590 <lambor590@gmail.com>
2025-03-15 17:50:09 +0100

    feat: add DirectXHook to global definitions

--- Mod/include/GlobalDefinitions.h
+++ Mod/include/GlobalDefinitions.h
@@ -2,6 +2,8 @@
 
 class GameHook;
 class ConfigManager;
+class DirectXHook;
 
 extern GameHook* g_GameHook;
-extern ConfigManager& g_ConfigManager; 
+extern ConfigManager& g_ConfigManager;
+extern DirectXHook* g_DirectXHook;
