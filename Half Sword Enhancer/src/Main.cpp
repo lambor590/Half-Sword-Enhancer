654176bec20df57edf472582ed684d04684f87a1
lambor590 <lambor590@gmail.com>
2025-03-28 21:44:10 +0100

    feat: refactor main function for improved structure
    - Replace console output with showBanner function
    - Simplify game process handling with findOrLaunchGame
    - Enhance DLL injection process with extractDllToTempFile and injectDll functions

--- Half Sword Enhancer/src/Main.cpp
+++ Half Sword Enhancer/src/Main.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <Windows.h>
-#include <string>
-#include <thread>
-#include <libloaderapi.h>
-
 #include "../resource/resource.h"
 #include "Util.h"
 #include "Updater.h"
@@ -13,120 +7,45 @@ using namespace Util;
 
 int main() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
-    std::cout << R"(
-        __  __      ______   _____                        __
-       / / / /___ _/ / __/  / ___/      ______  _________/ /
-      / /_/ / __ `/ / /_    \__ \ | /| / / __ \/ ___/ __  /
-     / __  / /_/ / / __/   ___/ / |/ |/ / /_/ / /  / /_/ /
-    /_/ /_/\__,_/_/_/     /____/|__/|__/\____/_/   \__,_/
-    )";
-
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-    std::cout << R"(
-        ______      __
-       / ____/___  / /_  ____ _____  ________  _____
-      / __/ / __ \/ __ \/ __ `/ __ \/ ___/ _ \/ ___/
-     / /___/ / / / / / / /_/ / / / / /__/  __/ /
-    /_____/_/ /_/_/ /_/\__,_/_/ /_/\___/\___/_/     
-    )" << "\n";
-
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    
+    showBanner(hConsole);
+    
     std::string windowTitle = "Half Sword Enhancer ";
     windowTitle += Updater::getLocalVersion();
     SetWindowText(GetConsoleWindow(), windowTitle.c_str());
-
+    
     const std::string& appDataPath = getAppDataPath();
     const char* processName = "VersionTest54-Win64-Shipping.exe";
     std::string dllPath = appDataPath + "\\temp_enhancer.dll";
-
+    
     Logger::info("Made by The Ghost");
-
+    
     if (isRunningAsAdmin())
         Logger::warn("Detected administrator privileges. Running as administrator can cause permission issues.");
-
+    
     Updater::checkForUpdates();
-
-    Logger::info("Searching for Half Sword process...");
-    DWORD processId = getProcessIdByName(processName);
-    if (processId == 0) {
-        Logger::info("Half Sword not found, launching it...");
-        ShellExecuteA(0, 0, "steam://rungameid/2527870", 0, 0, SW_SHOW);
-        while (!(processId = getProcessIdByName(processName))) {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-    }
-    Logger::info("Half Sword process found.");
-
+    
+    DWORD processId = findOrLaunchGame(processName);
+    
     if (!WaitForGameWindow(processId)) {
         fail("Could not find game window. Aborting injection.");
     }
-
-    HANDLE procHandle = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_CREATE_THREAD, FALSE, processId);
-    if (!procHandle) fail("Failed to open handle! Please run the launcher as administrator or check your antivirus.");
+    
+    ScopedHandle procHandle(OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_CREATE_THREAD, FALSE, processId));
+    if (!procHandle.isValid()) {
+        fail("Failed to open handle! Please run the launcher as administrator or check your antivirus.");
+    }
     Logger::info("Handle opened successfully!");
-
-    HRSRC hResource = FindResourceA(NULL, MAKEINTRESOURCE(IDR_DLL1), RT_RCDATA);
-    if (!hResource) fail("Failed to find mod resource!");
-
-    HGLOBAL hLoadedResource = LoadResource(NULL, hResource);
-    if (!hLoadedResource) fail("Failed to load mod resource!");
-
-    LPVOID pLockedResource = LockResource(hLoadedResource);
-    DWORD dwResourceSize = SizeofResource(NULL, hResource);
-    if (!pLockedResource || dwResourceSize == 0) fail("Failed to lock mod resource!");
-
-    HANDLE hFile = CreateFileA(dllPath.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) fail("Failed to create temporary DLL file.");
-
-    DWORD bytesWritten;
-    if (!WriteFile(hFile, pLockedResource, dwResourceSize, &bytesWritten, NULL) || bytesWritten != dwResourceSize) {
-        CloseHandle(hFile);
-        fail("Failed to write DLL to temporary file.");
-    }
-
-#pragma warning(suppress : 6001)
-    CloseHandle(hFile);
-    Logger::info("DLL written to temporary file successfully.");
-
-    LPVOID remoteMem = VirtualAllocEx(procHandle, nullptr, dllPath.length() + 1, MEM_COMMIT, PAGE_READWRITE);
-    if (!remoteMem) fail("Failed to allocate memory in Half Sword's process.");
-    else if (!WriteProcessMemory(procHandle, remoteMem, dllPath.c_str(), dllPath.length() + 1, NULL)) {
-        fail("Failed to write DLL path to Half Sword's process memory.");
-    }
-    Logger::info("DLL path written to Half Sword's process memory.");
-
-#pragma warning(suppress : 6387)
-    HANDLE threadHandle = CreateRemoteThread(procHandle, nullptr, 0,
-        (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA"), remoteMem, 0, nullptr);
-    if (!threadHandle) {
-        fail("Failed to create remote thread.");
-    }
-    else {
-        DWORD waitResult = WaitForSingleObject(threadHandle, 10000);
-        if (waitResult == WAIT_TIMEOUT) {
-            Logger::warn("Thread execution timed out. The game might be unresponsive.");
-            TerminateThread(threadHandle, 1);
-            CloseHandle(threadHandle);
-            VirtualFreeEx(procHandle, remoteMem, 0, MEM_RELEASE);
-            CloseHandle(procHandle);
-            DeleteFileA(dllPath.c_str());
-            fail("DLL injection timed out. Please try again with the game freshly launched.");
-        }
-        CloseHandle(threadHandle);
-    }
-    Logger::info("Remote thread created successfully.");
-
+    
+    extractDllToTempFile(dllPath, IDR_DLL1);
+    injectDll(procHandle.get(), dllPath);
+    
     DeleteFileA(dllPath.c_str());
     Logger::info("Temporary DLL file deleted.");
-
-    if (remoteMem) VirtualFreeEx(procHandle, remoteMem, 0, MEM_RELEASE);
-    Logger::info("Memory deallocated successfully.");
-
-    CloseHandle(procHandle);
+    
     Logger::info("Half Sword Enhancer injected successfully! Enjoy!");
     Logger::info("Exiting launcher in 3 seconds...");
     std::this_thread::sleep_for(std::chrono::seconds(3));
+    
     return 0;
 }
