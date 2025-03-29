e9c9f0ef987e56ae22f273fe99a23b5193936df4
lambor590 <lambor590@gmail.com>
2025-03-29 18:30:22 +0100

    feat: enhance resource management in Util.h
    - Introduce ScopedResource template for better handle management
    - Update WaitForGameWindow to accept a timeout parameter
    - Refactor DLL injection process for improved error handling

--- Half Sword Enhancer/include/Util.h
+++ Half Sword Enhancer/include/Util.h
@@ -9,40 +9,85 @@
 #include <iostream>
 #include <libloaderapi.h>
 #include <memory>
+#include <functional>
+#include <stdexcept>
 
 #include "Logger.h"
 
 namespace Util {
+    template<typename HandleType, typename CleanupFunc>
+    class ScopedResource {
+    private:
+        HandleType handle;
+        CleanupFunc cleanup;
+        bool released = false;
+
+    public:
+        ScopedResource(HandleType h, CleanupFunc cf) : handle(h), cleanup(cf) {}
+        
+        ~ScopedResource() { 
+            if (!released && isValid()) 
+                cleanup(handle); 
+        }
+        
+        operator HandleType() const { return handle; }
+        HandleType get() const { return handle; }
+        
+        HandleType release() { 
+            released = true; 
+            return handle; 
+        }
+        
+        bool isValid() const {
+            if constexpr (std::is_same_v<HandleType, HANDLE>)
+                return handle != NULL && handle != INVALID_HANDLE_VALUE;
+            else
+                return handle != NULL;
+        }
+
+        ScopedResource(const ScopedResource&) = delete;
+        ScopedResource& operator=(const ScopedResource&) = delete;
+
+        ScopedResource(ScopedResource&& other) noexcept 
+            : handle(other.handle), cleanup(other.cleanup), released(other.released) {
+            other.released = true;
+        }
+        
+        ScopedResource& operator=(ScopedResource&& other) noexcept {
+            if (this != &other) {
+                if (!released && isValid())
+                    cleanup(handle);
+                
+                handle = other.handle;
+                cleanup = other.cleanup;
+                released = other.released;
+                other.released = true;
+            }
+            return *this;
+        }
+    };
+
+    using ScopedHandle = ScopedResource<HANDLE, std::function<void(HANDLE)>>;
+    using ScopedVirtualMemory = ScopedResource<LPVOID, std::function<void(LPVOID)>>;
+
+    inline ScopedHandle createScopedHandle(HANDLE handle) {
+        return ScopedHandle(handle, [](HANDLE h) { CloseHandle(h); });
+    }
+
+    inline ScopedVirtualMemory createScopedVirtualMemory(HANDLE process, LPVOID memory) {
+        return ScopedVirtualMemory(memory, [process](LPVOID mem) { 
+            if (process && mem) 
+                VirtualFreeEx(process, mem, 0, MEM_RELEASE); 
+        });
+    }
+
     struct EnumWindowsData {
         DWORD processId;
         HWND windowHandle;
     };
 
-    class ScopedHandle {
-    private:
-        HANDLE handle;
-    public:
-        explicit ScopedHandle(HANDLE h) : handle(h) {}
-        ~ScopedHandle() { if (handle && handle != INVALID_HANDLE_VALUE) CloseHandle(handle); }
-        operator HANDLE() const { return handle; }
-        HANDLE get() const { return handle; }
-        HANDLE release() { HANDLE temp = handle; handle = nullptr; return temp; }
-        bool isValid() const { return handle && handle != INVALID_HANDLE_VALUE; }
-    };
-
-    class ScopedVirtualMemory {
-    private:
-        HANDLE process;
-        LPVOID memory;
-    public:
-        ScopedVirtualMemory(HANDLE proc, LPVOID mem) : process(proc), memory(mem) {}
-        ~ScopedVirtualMemory() { if (process && memory) VirtualFreeEx(process, memory, 0, MEM_RELEASE); }
-        LPVOID get() const { return memory; }
-        LPVOID release() { LPVOID temp = memory; memory = nullptr; return temp; }
-    };
-
     inline BOOL CALLBACK EnumWindowsCallback(HWND hwnd, LPARAM lParam) {
-        EnumWindowsData* data = reinterpret_cast<EnumWindowsData*>(lParam);
+        auto* data = reinterpret_cast<EnumWindowsData*>(lParam);
         DWORD windowProcessId;
         GetWindowThreadProcessId(hwnd, &windowProcessId);
 
@@ -59,14 +104,11 @@ namespace Util {
         return data.windowHandle;
     }
 
-    inline bool WaitForGameWindow(DWORD processId) {
+    inline bool WaitForGameWindow(DWORD processId, int timeoutSeconds = 60) {
         Logger::info("Waiting for Half Sword window to be available...");
 
-        for (int i = 0; i < 60; i++) {
-            HWND hwnd = FindProcessWindow(processId);
-            if (hwnd != NULL) {
-                char windowTitle[256];
-                GetWindowTextA(hwnd, windowTitle, sizeof(windowTitle));
+        for (int i = 0; i < timeoutSeconds; i++) {
+            if (HWND hwnd = FindProcessWindow(processId)) {
                 Logger::info("Half Sword window found!");
                 return true;
             }
@@ -76,25 +118,24 @@ namespace Util {
         return false;
     }
 
-    inline static DWORD getProcessIdByName(const char* processName) {
+    inline DWORD getProcessIdByName(const char* processName) {
         PROCESSENTRY32 processEntry{ sizeof(PROCESSENTRY32) };
-        HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-        if (snapshot == INVALID_HANDLE_VALUE) return 0;
+        auto snapshot = createScopedHandle(CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0));
+        
+        if (!snapshot.isValid()) 
+            return 0;
 
-        DWORD processId = 0;
         if (Process32First(snapshot, &processEntry)) {
             do {
-                if (_stricmp(processEntry.szExeFile, processName) == 0) {
-                    processId = processEntry.th32ProcessID;
-                    break;
-                }
+                if (_stricmp(processEntry.szExeFile, processName) == 0)
+                    return processEntry.th32ProcessID;
             } while (Process32Next(snapshot, &processEntry));
         }
-        CloseHandle(snapshot);
-        return processId;
+        
+        return 0;
     }
 
-    inline static const bool isRunningAsAdmin() {
+    inline bool isRunningAsAdmin() {
         BOOL isAdmin = FALSE;
         PSID adminGroup = NULL;
         SID_IDENTIFIER_AUTHORITY ntAuthority = SECURITY_NT_AUTHORITY;
@@ -105,58 +146,43 @@ namespace Util {
             FreeSid(adminGroup);
         }
 
-        return isAdmin;
+        return isAdmin != 0;
     }
 
-    inline static void fail(const char* msg) {
-        MessageBox(nullptr, msg, "Error", MB_ICONERROR);
+    class EnhancerException : public std::runtime_error {
+    public:
+        explicit EnhancerException(const std::string& message) : std::runtime_error(message) {}
+    };
+
+    [[noreturn]] inline void fail(const char* msg) {
+        MessageBoxA(nullptr, msg, "Error", MB_ICONERROR);
         exit(1);
     }
 
-    inline static const std::string& getAppDataPath() {
+    inline void showError(const char* msg) {
+        MessageBoxA(nullptr, msg, "Error", MB_ICONERROR);
+    }
+
+    inline const std::string& getAppDataPath() {
         static std::string fullPath;
 
         if (fullPath.empty()) {
             char appDataPath[MAX_PATH];
 
-            if (FAILED(SHGetFolderPathA(NULL, CSIDL_APPDATA, NULL, 0, appDataPath))) {
+            if (FAILED(SHGetFolderPathA(NULL, CSIDL_APPDATA, NULL, 0, appDataPath)))
                 fail("Failed to get AppData path");
-            }
 
             fullPath = std::string(appDataPath) + "\\Half Sword Enhancer";
 
-            if (!std::filesystem::exists(fullPath)) {
-                if (!std::filesystem::create_directory(fullPath))
-                    fail("Failed to create directory in AppData");
-            }
+            if (!std::filesystem::exists(fullPath) && 
+                !std::filesystem::create_directory(fullPath))
+                fail("Failed to create directory in AppData");
         }
 
         return fullPath;
     }
 
-    inline void showBanner(HANDLE hConsole) {
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
-        std::cout << R"(
-            __  __      ______   _____                        __
-           / / / /___ _/ / __/  / ___/      ______  _________/ /
-          / /_/ / __ `/ / /_    \__ \ | /| / / __ \/ ___/ __  /
-         / __  / /_/ / / __/   ___/ / |/ |/ / /_/ / /  / /_/ /
-        /_/ /_/\__,_/_/_/     /____/|__/|__/\____/_/   \__,_/
-        )";
-
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-        std::cout << R"(
-            ______      __
-           / ____/___  / /_  ____ _____  ________  _____
-          / __/ / __ \/ __ \/ __ `/ __ \/ ___/ _ \/ ___/
-         / /___/ / / / / / / /_/ / / / / /__/  __/ /
-        /_____/_/ /_/_/ /_/\__,_/_/ /_/\___/\___/_/     
-        )" << "\n";
-
-        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-    }
-
-    inline DWORD findOrLaunchGame(const char* processName) {
+    inline DWORD findOrLaunchGame(const char* processName, int timeoutSeconds = 60) {
         Logger::info("Searching for Half Sword process...");
         DWORD processId = getProcessIdByName(processName);
 
@@ -164,15 +190,11 @@ namespace Util {
             Logger::info("Half Sword not found, launching it...");
             ShellExecuteA(0, 0, "steam://rungameid/2527870", 0, 0, SW_SHOW);
 
-            for (int i = 0; i < 60; i++) {
-                processId = getProcessIdByName(processName);
-                if (processId != 0) break;
+            for (int i = 0; i < timeoutSeconds && (processId = getProcessIdByName(processName)) == 0; i++)
                 std::this_thread::sleep_for(std::chrono::seconds(1));
-            }
 
-            if (processId == 0) {
+            if (processId == 0)
                 fail("Timeout waiting for game to start");
-            }
         }
 
         Logger::info("Half Sword process found.");
@@ -181,50 +203,99 @@ namespace Util {
 
     inline void extractDllToTempFile(const std::string& dllPath, DWORD resourceId) {
         HRSRC hResource = FindResourceA(NULL, MAKEINTRESOURCE(resourceId), RT_RCDATA);
-        if (!hResource) fail("Failed to find mod resource!");
+        if (!hResource) 
+            fail("Failed to find mod resource!");
 
+        #pragma warning(suppress : 6387)
         HGLOBAL hLoadedResource = LoadResource(NULL, hResource);
-        if (!hLoadedResource) fail("Failed to load mod resource!");
+        if (!hLoadedResource) 
+            fail("Failed to load mod resource!");
 
+        #pragma warning(suppress : 6387)
         LPVOID pLockedResource = LockResource(hLoadedResource);
+        if (!pLockedResource) 
+            fail("Failed to lock mod resource!");
+        
+        #pragma warning(suppress : 6387)
         DWORD dwResourceSize = SizeofResource(NULL, hResource);
-        if (!pLockedResource || dwResourceSize == 0) fail("Failed to lock mod resource!");
+        if (dwResourceSize == 0) 
+            fail("Resource size is zero!");
 
-        ScopedHandle hFile(CreateFileA(dllPath.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL));
-        if (!hFile.isValid()) fail("Failed to create temporary DLL file.");
+        auto hFile = createScopedHandle(CreateFileA(
+            dllPath.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 
+            FILE_ATTRIBUTE_NORMAL, NULL
+        ));
+        
+        if (!hFile.isValid()) 
+            fail("Failed to create temporary DLL file.");
 
         DWORD bytesWritten;
-        if (!WriteFile(hFile.get(), pLockedResource, dwResourceSize, &bytesWritten, NULL) || bytesWritten != dwResourceSize) {
+        if (!WriteFile(hFile, pLockedResource, dwResourceSize, &bytesWritten, NULL) || 
+            bytesWritten != dwResourceSize)
             fail("Failed to write DLL to temporary file.");
-        }
 
         Logger::info("DLL written to temporary file successfully.");
     }
 
-    inline void injectDll(HANDLE procHandle, const std::string& dllPath) {
-        LPVOID remoteMem = VirtualAllocEx(procHandle, nullptr, dllPath.length() + 1, MEM_COMMIT, PAGE_READWRITE);
-        if (!remoteMem) fail("Failed to allocate memory in Half Sword's process.");
+    inline bool injectDll(DWORD processId, const std::string& dllPath, int timeoutMs = 10000) {
+        auto procHandle = createScopedHandle(OpenProcess(
+            PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_CREATE_THREAD, 
+            FALSE, processId
+        ));
+        
+        if (!procHandle.isValid()) {
+            showError("Failed to open handle! Please run the launcher as administrator or check your antivirus.");
+            return false;
+        }
+        
+        Logger::info("Handle opened successfully!");
 
-        ScopedVirtualMemory scopedMem(procHandle, remoteMem);
+        auto remoteMem = createScopedVirtualMemory(
+            procHandle, 
+            VirtualAllocEx(procHandle, nullptr, dllPath.length() + 1, MEM_COMMIT, PAGE_READWRITE)
+        );
+        
+        if (!remoteMem.isValid()) {
+            showError("Failed to allocate memory in Half Sword's process.");
+            return false;
+        }
 
         if (!WriteProcessMemory(procHandle, remoteMem, dllPath.c_str(), dllPath.length() + 1, NULL)) {
-            fail("Failed to write DLL path to Half Sword's process memory.");
+            showError("Failed to write DLL path to Half Sword's process memory.");
+            return false;
         }
-
+        
         Logger::info("DLL path written to Half Sword's process memory.");
 
-        ScopedHandle threadHandle(CreateRemoteThread(procHandle, nullptr, 0,
-            (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA"),
-            remoteMem, 0, nullptr));
-
-        if (!threadHandle.isValid()) fail("Failed to create remote thread.");
-
-        DWORD waitResult = WaitForSingleObject(threadHandle.get(), 10000);
-        if (waitResult == WAIT_TIMEOUT) {
-            Logger::warn("Thread execution timed out. The game might be unresponsive.");
-            fail("DLL injection timed out. Please try again with the game freshly launched.");
+        HMODULE kernel32Module = GetModuleHandleA("kernel32.dll");
+        FARPROC loadLibraryAddr = kernel32Module ? GetProcAddress(kernel32Module, "LoadLibraryA") : NULL;
+        
+        if (!loadLibraryAddr) {
+            showError("Failed to get LoadLibraryA address");
+            return false;
         }
 
-        Logger::info("Remote thread created successfully.");
+        #pragma warning(suppress : 6387 6001)
+        auto threadHandle = createScopedHandle(CreateRemoteThread(
+            procHandle, nullptr, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddr, 
+            remoteMem, 0, nullptr
+        ));
+        
+        if (!threadHandle.isValid()) {
+            showError("Failed to create remote thread.");
+            return false;
+        }
+
+        #pragma warning(suppress : 6001)
+        DWORD waitResult = WaitForSingleObject(threadHandle, timeoutMs);
+        if (waitResult == WAIT_TIMEOUT) {
+            Logger::warn("Thread execution timed out. The game might be unresponsive.");
+            #pragma warning(suppress : 6258)
+            TerminateThread(threadHandle, 1);
+            return false;
+        }
+
+        Logger::info("DLL injected successfully.");
+        return true;
     }
 }
