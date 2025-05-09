b252166606a6de9320def7ceaf3b1ebc9867fc7f
lambor590 <lambor590@gmail.com>
2025-05-09 19:12:42 +0200

    feat: add mouse buttons support
    - Updated key event handling to support multiple button types
    - Changed keybind storage to use integer keys instead of pointers
    - Improved keybind lookup efficiency

--- Mod/src/KeybindManager.cpp
+++ Mod/src/KeybindManager.cpp
@@ -6,7 +6,8 @@
 
 int KeybindManager::s_toggleGuiKey = VK_INSERT;
 int KeybindManager::s_unbindKey = VK_DELETE;
-std::unordered_map<int*, std::function<void()>> KeybindManager::s_keybinds;
+std::unordered_map<int, std::function<void()>> KeybindManager::s_keybinds;
+std::unordered_map<int*, int> KeybindManager::s_keyPtrMap;
 bool KeybindManager::s_initialized = false;
 
 void KeybindManager::Initialize() {
@@ -24,15 +25,28 @@ void KeybindManager::SaveKeybinds() {
 }
 
 bool KeybindManager::ProcessKeyEvent(UINT msg, WPARAM wParam) {
-    if (msg == WM_KEYDOWN) {
-        auto it = std::find_if(s_keybinds.begin(), s_keybinds.end(), 
-            [wParam](const auto& pair) { return *pair.first == wParam; });
-        
-        if (it != s_keybinds.end()) {
-            it->second();
-            return true;
+    int keyCode;
+    switch (msg) {
+        case WM_KEYDOWN:
+            keyCode = static_cast<int>(wParam);
+            break;
+        case WM_MBUTTONDOWN:
+        case WM_MBUTTONDBLCLK:
+            keyCode = VK_MBUTTON;
+            break;
+        case WM_XBUTTONDOWN:
+        case WM_XBUTTONDBLCLK: {
+            WORD xbutton = GET_XBUTTON_WPARAM(wParam);
+            keyCode = (xbutton == XBUTTON1 ? VK_XBUTTON1 : VK_XBUTTON2);
+            break;
         }
+        default:
+            return false;
     }
-    
+    auto it = s_keybinds.find(keyCode);
+    if (it != s_keybinds.end()) {
+        it->second();
+        return true;
+    }
     return false;
 } 
