c53b2c4219640f4bcfb99994088be01274e5efe6
lambor590 <lambor590@gmail.com>
2025-05-12 19:44:25 +0200

    refactor: simplify KeybindManager structure
    - Replaced unordered_map with vector for keybind storage
    - Updated method signatures to noexcept
    - Removed unused methods and variables
    - Streamlined key handling logic

--- Mod/include/KeybindManager.h
+++ Mod/include/KeybindManager.h
@@ -2,121 +2,36 @@
 
 #include <Windows.h>
 #include <functional>
-#include <unordered_map>
-#include <string>
+#include <vector>
+#include <utility>
 
-class Gui;
 class ConfigManager;
-
 extern ConfigManager& g_ConfigManager;
 
 class KeybindManager {
+public:
+    using Callback = std::function<void()>;
+
 private:
+    static Callback s_callbacks[256];
+    static std::vector<std::pair<int*, Callback>> s_bindingList;
+    static bool s_initialized;
     static int s_toggleGuiKey;
     static int s_unbindKey;
-    static std::unordered_map<int, std::function<void()>> s_keybinds;
-    static std::unordered_map<int*, int> s_keyPtrMap;
-    static bool s_initialized;
 
 public:
-    static void Initialize();
+    static void Initialize() noexcept;
 
-    static const char* GetKeyName(int vKey) {
-        static const std::unordered_map<int, const char*> keyNameMap = {
-            {-1, "Unbound"}, {VK_LSHIFT, "Left Shift"}, {VK_RSHIFT, "Right Shift"}, {VK_SHIFT, "Shift"},
-            {VK_CONTROL, "Control"}, {VK_LCONTROL, "Left Control"}, {VK_RCONTROL, "Right Control"},
-            {VK_MENU, "Alt"}, {VK_LMENU, "Left Alt"}, {VK_RMENU, "Right Alt"},
-            {VK_BACK, "Backspace"}, {VK_TAB, "Tab"}, {VK_RETURN, "Enter"},
-            {VK_SPACE, "Space"}, {VK_CAPITAL, "Caps Lock"}, {VK_ESCAPE, "Escape"},
-            {VK_LEFT, "Left"}, {VK_UP, "Up"}, {VK_RIGHT, "Right"}, {VK_DOWN, "Down"},
-            {VK_DELETE, "Delete"}, {VK_INSERT, "Insert"}, {VK_HOME, "Home"}, {VK_END, "End"},
-            {VK_PRIOR, "Page Up"}, {VK_NEXT, "Page Down"}, {VK_SNAPSHOT, "Print Screen"},
-            {VK_SCROLL, "Scroll Lock"}, {VK_PAUSE, "Pause"}, {VK_NUMLOCK, "Num Lock"},
-            {VK_NUMPAD0, "Numpad 0"}, {VK_NUMPAD1, "Numpad 1"}, {VK_NUMPAD2, "Numpad 2"},
-            {VK_NUMPAD3, "Numpad 3"}, {VK_NUMPAD4, "Numpad 4"}, {VK_NUMPAD5, "Numpad 5"},
-            {VK_NUMPAD6, "Numpad 6"}, {VK_NUMPAD7, "Numpad 7"}, {VK_NUMPAD8, "Numpad 8"},
-            {VK_NUMPAD9, "Numpad 9"}, {VK_MULTIPLY, "Numpad *"}, {VK_ADD, "Numpad +"},
-            {VK_SUBTRACT, "Numpad -"}, {VK_DECIMAL, "Numpad ."}, {VK_DIVIDE, "Numpad /"},
-            {VK_OEM_1, ";"}, {VK_OEM_PLUS, "="}, {VK_OEM_COMMA, ","}, {VK_OEM_MINUS, "-"},
-            {VK_OEM_PERIOD, "."}, {VK_OEM_2, "/"}, {VK_OEM_3, "`"}, {VK_OEM_4, "["},
-            {VK_OEM_5, "\\"}, {VK_OEM_6, "]"}, {VK_OEM_7, "'"},
-            {VK_MBUTTON, "MMB"}, {VK_XBUTTON1, "Mouse 4"},
-            {VK_XBUTTON2, "Mouse 5"}
-        };
+    static void RegisterKeybind(int* keyPtr, Callback callback) noexcept;
+    static void UnregisterKeybind(int* keyPtr) noexcept;
+    static void UpdateBindings() noexcept;
+    static bool HandleKeyPress(bool& waitingForKey, int& key) noexcept;
 
-        if (auto it = keyNameMap.find(vKey); it != keyNameMap.end())
-            return it->second;
+    static const char* GetKeyName(int vKey) noexcept;
 
-        static char singleChar[2] = { 0 };
-        if ((vKey >= '0' && vKey <= '9') || (vKey >= 'A' && vKey <= 'Z')) {
-            singleChar[0] = static_cast<char>(vKey);
-            singleChar[1] = '\0';
-            return singleChar;
-        }
+    static bool ProcessKeyEvent(UINT msg, WPARAM wParam) noexcept;
 
-        static char fKeyName[4] = { 0 };
-        if (vKey >= VK_F1 && vKey <= VK_F12) {
-            sprintf_s(fKeyName, "F%d", vKey - VK_F1 + 1);
-            return fKeyName;
-        }
-
-        static char keyName[32];
-        UINT scanCode = MapVirtualKey(vKey, MAPVK_VK_TO_VSC);
-        return GetKeyNameTextA(scanCode << 16, keyName, sizeof(keyName)) > 0 ? keyName : "Unknown";
-    }
-
-    static bool HandleKeyPress(bool& waitingForKey, int& key) {
-        if (!waitingForKey) return false;
-
-        static bool keyPressed[256] = { false };
-
-        for (int i = 0; i < 256; i++) {
-            if (GetAsyncKeyState(i) & 0x8000) {
-                keyPressed[i] = true;
-            } else if (keyPressed[i]) {
-                key = i == s_unbindKey ? -1 : i;
-                waitingForKey = false;
-                keyPressed[i] = false;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    static void RegisterKeybind(int* keyPtr, std::function<void()> callback) {
-        auto it = s_keyPtrMap.find(keyPtr);
-        if (it != s_keyPtrMap.end()) {
-            s_keybinds.erase(it->second);
-            s_keyPtrMap.erase(it);
-        }
-        int code = *keyPtr;
-        if (code != -1) {
-            s_keybinds[code] = callback;
-            s_keyPtrMap[keyPtr] = code;
-        }
-    }
-
-    static void UnregisterKeybind(int* keyPtr) {
-        auto it = s_keyPtrMap.find(keyPtr);
-        if (it != s_keyPtrMap.end()) {
-            s_keybinds.erase(it->second);
-            s_keyPtrMap.erase(it);
-        }
-    }
-
-    static int& GetToggleGuiKey() {
-        return s_toggleGuiKey;
-    }
-
-    static int& GetUnbindKey() {
-        return s_unbindKey;
-    }
-
-    static void SaveKeybinds();
-
-    static bool ProcessKeyEvent(UINT msg, WPARAM wParam);
-
-    static std::unordered_map<int, std::function<void()>>& GetKeybinds() {
-        return s_keybinds;
-    }
+    static int& GetToggleGuiKey() noexcept { return s_toggleGuiKey; }
+    static int& GetUnbindKey() noexcept { return s_unbindKey; }
+    static void SaveKeybinds() noexcept;
 };
