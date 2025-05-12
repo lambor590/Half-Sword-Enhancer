1300b966663e4e520b6e60bff37bc5ba6b0e5338
lambor590 <lambor590@gmail.com>
2025-05-12 20:07:05 +0200

    feat: improve key name initialization and binding logic
    - Refactored key name table initialization for clarity
    - Added handling for unbound keys and digit/letter names
    - Updated binding update logic to clear previous bindings

--- Mod/src/KeybindManager.cpp
+++ Mod/src/KeybindManager.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <cstdio>
 
 #include "KeybindManager.h"
 #include "ConfigManager.h"
@@ -10,76 +9,85 @@ std::vector<std::pair<int*, KeybindManager::Callback>> KeybindManager::s_binding
 bool KeybindManager::s_initialized = false;
 int KeybindManager::s_toggleGuiKey = VK_INSERT;
 int KeybindManager::s_unbindKey = VK_DELETE;
+std::vector<int> KeybindManager::s_boundCodes;
 
-// Static lookup table for key names to optimize runtime resolution
-static const char* s_keyNameTable[256];
+const char* KeybindManager::s_keyNameTable[256];
 static void InitKeyNameTable() noexcept {
-    for (int i = 0; i < 256; ++i) s_keyNameTable[i] = nullptr;
-    s_keyNameTable[VK_LSHIFT]      = "Left Shift";
-    s_keyNameTable[VK_RSHIFT]      = "Right Shift";
-    s_keyNameTable[VK_SHIFT]       = "Shift";
-    s_keyNameTable[VK_CONTROL]     = "Control";
-    s_keyNameTable[VK_LCONTROL]    = "Left Control";
-    s_keyNameTable[VK_RCONTROL]    = "Right Control";
-    s_keyNameTable[VK_MENU]        = "Alt";
-    s_keyNameTable[VK_LMENU]       = "Left Alt";
-    s_keyNameTable[VK_RMENU]       = "Right Alt";
-    s_keyNameTable[VK_BACK]        = "Backspace";
-    s_keyNameTable[VK_TAB]         = "Tab";
-    s_keyNameTable[VK_RETURN]      = "Enter";
-    s_keyNameTable[VK_SPACE]       = "Space";
-    s_keyNameTable[VK_CAPITAL]     = "Caps Lock";
-    s_keyNameTable[VK_ESCAPE]      = "Escape";
-    s_keyNameTable[VK_LEFT]        = "Left";
-    s_keyNameTable[VK_UP]          = "Up";
-    s_keyNameTable[VK_RIGHT]       = "Right";
-    s_keyNameTable[VK_DOWN]        = "Down";
-    s_keyNameTable[VK_DELETE]      = "Delete";
-    s_keyNameTable[VK_INSERT]      = "Insert";
-    s_keyNameTable[VK_HOME]        = "Home";
-    s_keyNameTable[VK_END]         = "End";
-    s_keyNameTable[VK_PRIOR]       = "Page Up";
-    s_keyNameTable[VK_NEXT]        = "Page Down";
-    s_keyNameTable[VK_SNAPSHOT]    = "Print Screen";
-    s_keyNameTable[VK_SCROLL]      = "Scroll Lock";
-    s_keyNameTable[VK_PAUSE]       = "Pause";
-    s_keyNameTable[VK_NUMLOCK]     = "Num Lock";
-    s_keyNameTable[VK_NUMPAD0]     = "Numpad 0";
-    s_keyNameTable[VK_NUMPAD1]     = "Numpad 1";
-    s_keyNameTable[VK_NUMPAD2]     = "Numpad 2";
-    s_keyNameTable[VK_NUMPAD3]     = "Numpad 3";
-    s_keyNameTable[VK_NUMPAD4]     = "Numpad 4";
-    s_keyNameTable[VK_NUMPAD5]     = "Numpad 5";
-    s_keyNameTable[VK_NUMPAD6]     = "Numpad 6";
-    s_keyNameTable[VK_NUMPAD7]     = "Numpad 7";
-    s_keyNameTable[VK_NUMPAD8]     = "Numpad 8";
-    s_keyNameTable[VK_NUMPAD9]     = "Numpad 9";
-    s_keyNameTable[VK_MULTIPLY]    = "Numpad *";
-    s_keyNameTable[VK_ADD]         = "Numpad +";
-    s_keyNameTable[VK_SUBTRACT]    = "Numpad -";
-    s_keyNameTable[VK_DECIMAL]     = "Numpad .";
-    s_keyNameTable[VK_DIVIDE]      = "Numpad /";
-    s_keyNameTable[VK_OEM_1]       = ";";
-    s_keyNameTable[VK_OEM_PLUS]    = "=";
-    s_keyNameTable[VK_OEM_COMMA]   = ",";
-    s_keyNameTable[VK_OEM_MINUS]   = "-";
-    s_keyNameTable[VK_OEM_PERIOD]  = ".";
-    s_keyNameTable[VK_OEM_2]       = "/";
-    s_keyNameTable[VK_OEM_3]       = "`";
-    s_keyNameTable[VK_OEM_4]       = "[";
-    s_keyNameTable[VK_OEM_5]       = "\\";
-    s_keyNameTable[VK_OEM_6]       = "]";
-    s_keyNameTable[VK_OEM_7]       = "'";
-    s_keyNameTable[VK_MBUTTON]     = "MMB";
-    s_keyNameTable[VK_XBUTTON1]    = "Mouse 4";
-    s_keyNameTable[VK_XBUTTON2]    = "Mouse 5";
+    for (int i = 0; i < 256; ++i) KeybindManager::s_keyNameTable[i] = "Unknown";
+    KeybindManager::s_keyNameTable[static_cast<unsigned char>(-1)] = "Unbound";
+    KeybindManager::s_keyNameTable[VK_LSHIFT]      = "Left Shift";
+    KeybindManager::s_keyNameTable[VK_RSHIFT]      = "Right Shift";
+    KeybindManager::s_keyNameTable[VK_SHIFT]       = "Shift";
+    KeybindManager::s_keyNameTable[VK_CONTROL]     = "Control";
+    KeybindManager::s_keyNameTable[VK_LCONTROL]    = "Left Control";
+    KeybindManager::s_keyNameTable[VK_RCONTROL]    = "Right Control";
+    KeybindManager::s_keyNameTable[VK_MENU]        = "Alt";
+    KeybindManager::s_keyNameTable[VK_LMENU]       = "Left Alt";
+    KeybindManager::s_keyNameTable[VK_RMENU]       = "Right Alt";
+    KeybindManager::s_keyNameTable[VK_BACK]        = "Backspace";
+    KeybindManager::s_keyNameTable[VK_TAB]         = "Tab";
+    KeybindManager::s_keyNameTable[VK_RETURN]      = "Enter";
+    KeybindManager::s_keyNameTable[VK_SPACE]       = "Space";
+    KeybindManager::s_keyNameTable[VK_CAPITAL]     = "Caps Lock";
+    KeybindManager::s_keyNameTable[VK_ESCAPE]      = "Escape";
+    KeybindManager::s_keyNameTable[VK_LEFT]        = "Left";
+    KeybindManager::s_keyNameTable[VK_UP]          = "Up";
+    KeybindManager::s_keyNameTable[VK_RIGHT]       = "Right";
+    KeybindManager::s_keyNameTable[VK_DOWN]        = "Down";
+    KeybindManager::s_keyNameTable[VK_DELETE]      = "Delete";
+    KeybindManager::s_keyNameTable[VK_INSERT]      = "Insert";
+    KeybindManager::s_keyNameTable[VK_HOME]        = "Home";
+    KeybindManager::s_keyNameTable[VK_END]         = "End";
+    KeybindManager::s_keyNameTable[VK_PRIOR]       = "Page Up";
+    KeybindManager::s_keyNameTable[VK_NEXT]        = "Page Down";
+    KeybindManager::s_keyNameTable[VK_SNAPSHOT]    = "Print Screen";
+    KeybindManager::s_keyNameTable[VK_SCROLL]      = "Scroll Lock";
+    KeybindManager::s_keyNameTable[VK_PAUSE]       = "Pause";
+    KeybindManager::s_keyNameTable[VK_NUMLOCK]     = "Num Lock";
+    KeybindManager::s_keyNameTable[VK_NUMPAD0]     = "Numpad 0";
+    KeybindManager::s_keyNameTable[VK_NUMPAD1]     = "Numpad 1";
+    KeybindManager::s_keyNameTable[VK_NUMPAD2]     = "Numpad 2";
+    KeybindManager::s_keyNameTable[VK_NUMPAD3]     = "Numpad 3";
+    KeybindManager::s_keyNameTable[VK_NUMPAD4]     = "Numpad 4";
+    KeybindManager::s_keyNameTable[VK_NUMPAD5]     = "Numpad 5";
+    KeybindManager::s_keyNameTable[VK_NUMPAD6]     = "Numpad 6";
+    KeybindManager::s_keyNameTable[VK_NUMPAD7]     = "Numpad 7";
+    KeybindManager::s_keyNameTable[VK_NUMPAD8]     = "Numpad 8";
+    KeybindManager::s_keyNameTable[VK_NUMPAD9]     = "Numpad 9";
+    KeybindManager::s_keyNameTable[VK_MULTIPLY]    = "Numpad *";
+    KeybindManager::s_keyNameTable[VK_ADD]         = "Numpad +";
+    KeybindManager::s_keyNameTable[VK_SUBTRACT]    = "Numpad -";
+    KeybindManager::s_keyNameTable[VK_DECIMAL]     = "Numpad .";
+    KeybindManager::s_keyNameTable[VK_DIVIDE]      = "Numpad /";
+    KeybindManager::s_keyNameTable[VK_OEM_1]       = ";";
+    KeybindManager::s_keyNameTable[VK_OEM_PLUS]    = "=";
+    KeybindManager::s_keyNameTable[VK_OEM_COMMA]   = ",";
+    KeybindManager::s_keyNameTable[VK_OEM_MINUS]   = "-";
+    KeybindManager::s_keyNameTable[VK_OEM_PERIOD]  = ".";
+    KeybindManager::s_keyNameTable[VK_OEM_2]       = "/";
+    KeybindManager::s_keyNameTable[VK_OEM_3]       = "`";
+    KeybindManager::s_keyNameTable[VK_OEM_4]       = "[";
+    KeybindManager::s_keyNameTable[VK_OEM_5]       = "\\";
+    KeybindManager::s_keyNameTable[VK_OEM_6]       = "]";
+    KeybindManager::s_keyNameTable[VK_OEM_7]       = "'";
+    KeybindManager::s_keyNameTable[VK_MBUTTON]     = "MMB";
+    KeybindManager::s_keyNameTable[VK_XBUTTON1]    = "Mouse 4";
+    KeybindManager::s_keyNameTable[VK_XBUTTON2]    = "Mouse 5";
+    static const char* digitNames[10] = {"0","1","2","3","4","5","6","7","8","9"};
+    for (int i = 0; i < 10; ++i) KeybindManager::s_keyNameTable['0' + i] = digitNames[i];
+    static const char* letterNames[26] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    for (int i = 0; i < 26; ++i) KeybindManager::s_keyNameTable['A' + i] = letterNames[i];
+    static const char* fKeyNames[12] = {"F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12"};
+    for (int i = 0; i < 12; ++i) KeybindManager::s_keyNameTable[VK_F1 + i] = fKeyNames[i];
 }
+
 static const struct KeyNameTableInitializer {
     KeyNameTableInitializer() { InitKeyNameTable(); }
 } s_keyNameTableInitializer;
 
 void KeybindManager::Initialize() noexcept {
     if (!s_initialized) {
+        s_bindingList.reserve(16);
         s_toggleGuiKey = g_ConfigManager.GetInt("Keybinds", "toggle_gui_key", VK_INSERT);
         s_unbindKey = g_ConfigManager.GetInt("Keybinds", "unbind_key", VK_DELETE);
         s_initialized = true;
@@ -114,12 +122,17 @@ void KeybindManager::UnregisterKeybind(int* keyPtr) noexcept {
 }
 
 void KeybindManager::UpdateBindings() noexcept {
-    for (auto& cb : s_callbacks)
-        cb = nullptr;
+    for (int code : s_boundCodes) {
+        if (code >= 0 && code < 256)
+            s_callbacks[code] = nullptr;
+    }
+    s_boundCodes.clear();
     for (auto& binding : s_bindingList) {
         int code = *binding.first;
-        if (code >= 0 && code < 256)
+        if (code >= 0 && code < 256) {
             s_callbacks[code] = binding.second;
+            s_boundCodes.push_back(code);
+        }
     }
 }
 
@@ -173,27 +186,4 @@ void KeybindManager::SaveKeybinds() noexcept {
     g_ConfigManager.SetInt("Keybinds", "toggle_gui_key", s_toggleGuiKey);
     g_ConfigManager.SetInt("Keybinds", "unbind_key", s_unbindKey);
     g_ConfigManager.SaveConfig();
-}
-
-const char* KeybindManager::GetKeyName(int vKey) noexcept {
-    if (vKey == -1) return "Unbound";
-    unsigned uv = static_cast<unsigned>(vKey);
-    if (uv < 256) {
-        const char* name = s_keyNameTable[uv];
-        if (name) return name;
-    }
-    if ((vKey >= '0' && vKey <= '9') || (vKey >= 'A' && vKey <= 'Z')) {
-        static char singleChar[2] = {0};
-        singleChar[0] = static_cast<char>(vKey);
-        singleChar[1] = '\0';
-        return singleChar;
-    }
-    if (vKey >= VK_F1 && vKey <= VK_F12) {
-        static char fKeyName[4] = {0};
-        sprintf_s(fKeyName, "F%d", vKey - VK_F1 + 1);
-        return fKeyName;
-    }
-    static char keyName[32];
-    UINT scanCode = MapVirtualKey(vKey, MAPVK_VK_TO_VSC);
-    return GetKeyNameTextA(scanCode << 16, keyName, sizeof(keyName)) > 0 ? keyName : "Unknown";
 } 
