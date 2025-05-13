cf041e1f32dc782543b85b3be4c4e2d056508da2
lambor590 <lambor590@gmail.com>
2025-05-13 18:55:48 +0200

    feat: enhance CollapsibleSection rendering with MedievalStyle
    - Added style push/pop for header rendering
    - Improved spacing and indentation for function rendering
    - Ensured header is always open on first render

--- Mod/src/CollapsibleSection.cpp
+++ Mod/src/CollapsibleSection.cpp
@@ -2,12 +2,32 @@
 
 #include "Menu/IMenuFunction.h"
 #include "Menu/ICollapsibleSection.h"
+#include "Gui.h"
 
 void CollapsibleSection::Render() {
-    if (ImGui::CollapsingHeader(name.c_str())) {
+    MedievalStyle::PushHeaderStyle();
+
+    ImGui::SetNextItemOpen(true, ImGuiCond_Once);
+    bool isOpen = ImGui::CollapsingHeader(name.c_str());
+    
+    MedievalStyle::PopHeaderStyle();
+    
+    if (isOpen) {
+        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(8, 6));
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(10, 8));
+        ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 25.0f);
+        
+        ImGui::Indent(10.0f);
+        
+        ImGui::Spacing();
+        
         for (auto& function : functions) {
             function->Render();
+            ImGui::Spacing();
         }
+        
+        ImGui::Unindent(10.0f);
+        ImGui::PopStyleVar(3);
     }
 }
 
