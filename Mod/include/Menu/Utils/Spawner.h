70d97305b25f2d8796326bd07e6c474b551a0db6
lambor590 <lambor590@gmail.com>
2025-05-27 17:07:13 +0200

    refactor: enhance actor spawning with callback support
    - Added SpawnRequest struct for better spawn management
    - Updated SpawnActor function to include a callback parameter
    - Introduced ProcessSpawnQueue function for handling spawn requests

--- Mod/include/Menu/Utils/Spawner.h
+++ Mod/include/Menu/Utils/Spawner.h
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <queue>
+#include <mutex>
+#include <functional>
+
 #include "SDK/CoreUObject_classes.hpp"
 #include "SDK/Engine_classes.hpp"
 
 namespace Spawner {
-    SDK::AActor* SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform);
+    struct SpawnRequest {
+        const SDK::UWorld* world;
+        std::string classPath;
+        SDK::FTransform transform;
+        std::function<void(SDK::AActor*)> callback;
+    };
+
+    void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback = nullptr);
+    void ProcessSpawnQueue();
 }
