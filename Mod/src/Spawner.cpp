1660e47d81f627247ec0189e158068ea3175cb04
lambor590 <lambor590@gmail.com>
2025-05-27 17:07:35 +0200

    refactor: implement spawn queue for asynchronous actor spawning
    - Added static queue and mutex for managing spawn requests
    - Created SpawnActorInternal for internal spawning logic
    - Introduced ProcessSpawnQueue to handle queued requests

--- Mod/src/Spawner.cpp
+++ Mod/src/Spawner.cpp
@@ -1,7 +1,13 @@
+#include <queue>
+#include <mutex>
+
 #include "Menu/Utils/Spawner.h"
 
 namespace Spawner {
-    SDK::AActor* SpawnActor(const SDK::UWorld* world, const std::string& classPath, const SDK::FTransform& transform) {
+    static std::queue<SpawnRequest> spawnQueue;
+    static std::mutex queueMutex;
+
+    static SDK::AActor* SpawnActorInternal(const SDK::UWorld* world, const std::string& classPath, const SDK::FTransform& transform) {
         std::wstring wClassName(classPath.begin(), classPath.end());
         SDK::FString classPathFStr(wClassName.c_str());
         SDK::FSoftClassPath softClassPath = SDK::UKismetSystemLibrary::MakeSoftClassPath(classPathFStr);
@@ -17,5 +23,26 @@ namespace Spawner {
         );
         SDK::UGameplayStatics::FinishSpawningActor(actor, transform, SDK::ESpawnActorScaleMethod::SelectDefaultAtRuntime);
         return actor;
+    }
+
+    void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback) {
+        std::lock_guard<std::mutex> lock(queueMutex);
+        spawnQueue.push({world, className, transform, callback});
+    }
+
+    void ProcessSpawnQueue() {
+        std::lock_guard<std::mutex> lock(queueMutex);
+        
+        while (!spawnQueue.empty()) {
+            const auto& request = spawnQueue.front();
+            
+            SDK::AActor* spawnedActor = SpawnActorInternal(request.world, request.classPath, request.transform);
+            
+            if (request.callback) {
+                request.callback(spawnedActor);
+            }
+            
+            spawnQueue.pop();
+        }
     }
 }
