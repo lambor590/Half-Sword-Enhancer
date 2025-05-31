1a456538bb6788872a210c6e24ab57e1a36097d0
lambor590 <lambor590@gmail.com>
2025-05-31 20:36:36 +0200

    refactor: enhance Renderer class with inline functions and alignment
    - Added inline specifiers for performance improvements
    - Introduced CACHE_ALIGN for better memory alignment
    - Updated function signatures for consistency and noexcept usage
    - Simplified command queue handling methods

--- Mod/include/Render/Renderer.h
+++ Mod/include/Render/Renderer.h
@@ -20,27 +20,38 @@
 constexpr UINT MAX_BUFFERS = 8;
 constexpr UINT TIMEOUT_MS = 500;
 
+#define LIKELY [[likely]]
+#define UNLIKELY [[unlikely]]
+#define CACHE_ALIGN __declspec(align(64))
+
 class Renderer : public ID3DRenderer
 {
 public:
     void OnPresent(IDXGISwapChain* pThis, UINT syncInterval, UINT flags) override;
     void OnResizeBuffers(IDXGISwapChain* pThis, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags) override;
-    void SetCommandQueue(ID3D12CommandQueue* commandQueue);
-    void SetGetCommandQueueCallback(void (*callback)());
+    
+    __forceinline void SetCommandQueue(ID3D12CommandQueue* commandQueue) noexcept {
+        this->commandQueue = commandQueue;
+    }
+    
+    __forceinline void SetGetCommandQueueCallback(void (*callback)()) noexcept {
+        commandQueueCallback = callback;
+    }
+    
     void Cleanup();
 
 private:
     Logger logger{ "Renderer" };
     IRenderCallback* GUI = &Gui::Get();
     
-    struct {
+    CACHE_ALIGN struct {
         HWND handle = 0;
         int width = 0, height = 0;
         D3D11_VIEWPORT viewport = {};
         bool viewportDirty = true;
     } window;
 
-    struct {
+    CACHE_ALIGN struct {
         bool mustInitResources = true;
         bool isD3D12 = false;
         bool GUIInitialized = false;
@@ -48,7 +59,7 @@ private:
         UINT bufferCount = 0;
     } state;
 
-    Microsoft::WRL::ComPtr<ID3D12Device> d3d12Device;
+    CACHE_ALIGN Microsoft::WRL::ComPtr<ID3D12Device> d3d12Device;
     Microsoft::WRL::ComPtr<ID3D11Device> d3d11Device;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d11Context;
     Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
@@ -58,7 +69,7 @@ private:
     Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvHeap;
     Microsoft::WRL::ComPtr<ID3D12Fence> fence;
     
-    std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, MAX_BUFFERS> d3d12RenderTargets;
+    CACHE_ALIGN std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, MAX_BUFFERS> d3d12RenderTargets;
     std::array<Microsoft::WRL::ComPtr<ID3D11Resource>, MAX_BUFFERS> d3d11WrappedBackBuffers;
     std::array<Microsoft::WRL::ComPtr<ID3D11RenderTargetView>, MAX_BUFFERS> d3d11RenderTargetViews;
     
@@ -66,13 +77,14 @@ private:
     HANDLE fenceEvent = nullptr;
     void (*commandQueueCallback)() = nullptr;
 
-    void RenderFrame();
-    bool InitD3DResources(IDXGISwapChain* swapChain);
-    bool InitD3D11();
-    bool InitD3D12();
-    void UpdateViewport();
-    bool SignalAndWait();
-    void ReleaseResources();
+    __forceinline void RenderFrame() noexcept;
+    __forceinline void UpdateViewport() noexcept;
+    __forceinline bool SignalAndWait() noexcept;
+    
+    bool InitD3DResources(IDXGISwapChain* swapChain) noexcept;
+    bool InitD3D11() noexcept;
+    bool InitD3D12() noexcept;
+    void ReleaseResources() noexcept;
     
     template<typename T>
     constexpr bool IsValid(T handle) const noexcept {
@@ -82,4 +94,8 @@ private:
             return handle != nullptr;
         }
     }
+    
+    __forceinline bool IsD3D12Path() const noexcept { return state.isD3D12; }
+    __forceinline bool NeedsReinitialization() const noexcept { return state.mustInitResources; }
+    __forceinline void MarkForReinitialization() noexcept { state.mustInitResources = true; }
 };
