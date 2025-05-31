c3257e2ce25078c140b9a6ffc29b4e63d9101f7f
lambor590 <lambor590@gmail.com>
2025-05-31 20:11:52 +0200

    refactor: update Renderer class for improved resource management
    - Replaced std::vector with std::array for render target management
    - Simplified member variables and initialization logic
    - Introduced constexpr variables for buffer limits and timeouts
    - Enhanced function signatures for better clarity and performance

--- Mod/include/Render/Renderer.h
+++ Mod/include/Render/Renderer.h
@@ -6,8 +6,8 @@
 #include <d3d11on12.h>
 #include <dxgi1_4.h>
 #include <wrl/client.h>
-#include <vector>
-#include <functional>
+#include <array>
+#include <type_traits>
 
 #include "ID3DRenderer.h"
 #include "IRenderCallback.h"
@@ -17,13 +17,12 @@
 #include "imgui/backends/imgui_impl_dx11.h"
 #include "imgui/backends/imgui_impl_win32.h"
 
-// D3D11 renderer with support for D3D12 using D3D11On12
+constexpr UINT MAX_BUFFERS = 8;
+constexpr UINT TIMEOUT_MS = 500;
+
 class Renderer : public ID3DRenderer
 {
 public:
-    Renderer() = default;
-    ~Renderer() = default;
-
     void OnPresent(IDXGISwapChain* pThis, UINT syncInterval, UINT flags) override;
     void OnResizeBuffers(IDXGISwapChain* pThis, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags) override;
     void SetCommandQueue(ID3D12CommandQueue* commandQueue);
@@ -32,84 +31,55 @@ public:
 
 private:
     Logger logger{ "Renderer" };
-
+    IRenderCallback* GUI = &Gui::Get();
+    
     struct {
         HWND handle = 0;
-        int width = 0;
-        int height = 0;
+        int width = 0, height = 0;
         D3D11_VIEWPORT viewport = {};
-        D3D11_VIEWPORT cachedViewport = {};
+        bool viewportDirty = true;
     } window;
-
-    IRenderCallback* GUI = &Gui::Get();
-    bool GUIInitialized = false;
 
     struct {
         bool mustInitResources = true;
-        bool firstTimeInitialized = false;
-        bool deviceRetrieved = false;
         bool isD3D12 = false;
-        bool commandQueueCallbackCalled = false;
+        bool GUIInitialized = false;
         UINT bufferIndex = 0;
         UINT bufferCount = 0;
-        DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
     } state;
 
-    struct {
-        Microsoft::WRL::ComPtr<ID3D12Device> d3d12Device;
-        Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d11Context;
-        Microsoft::WRL::ComPtr<ID3D11Device> d3d11Device;
-        Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
-        Microsoft::WRL::ComPtr<ID3D11On12Device> d3d11On12Device;
-        Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain;
-        Microsoft::WRL::ComPtr<IDXGISwapChain3> swapChain3;
-    } devices;
-
-    struct {
-        std::vector<Microsoft::WRL::ComPtr<ID3D12Resource>> d3d12RenderTargets;
-        std::vector<Microsoft::WRL::ComPtr<ID3D11Resource>> d3d11WrappedBackBuffers;
-        std::vector<Microsoft::WRL::ComPtr<ID3D11RenderTargetView>> d3d11RenderTargetViews;
-        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvHeap;
-        UINT rtvDescriptorSize = 0;
-    } resources;
-
-    struct {
-        Microsoft::WRL::ComPtr<ID3D12Fence> fence;
-        UINT64 currentValue = 0;
-        HANDLE event = nullptr;
-    } fenceSync;
+    Microsoft::WRL::ComPtr<ID3D12Device> d3d12Device;
+    Microsoft::WRL::ComPtr<ID3D11Device> d3d11Device;
+    Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d11Context;
+    Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue;
+    Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain;
+    Microsoft::WRL::ComPtr<IDXGISwapChain3> swapChain3;
+    Microsoft::WRL::ComPtr<ID3D11On12Device> d3d11On12Device;
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvHeap;
+    Microsoft::WRL::ComPtr<ID3D12Fence> fence;
     
-    std::function<void()> commandQueueCallback = nullptr;
-
-    bool InitD3DResources(IDXGISwapChain* swapChain);
-    bool RetrieveD3DDeviceFromSwapChain();
-    bool InitD3D();
-    bool InitD3D11();
-    bool InitD3D11Device();
-    bool InitD3D12();
-    bool InitD3D12Device();
-    bool CreateRtvDescriptorHeap();
-    bool CreateD3D11On12Device();
-    bool CreateD3D12Resources();
-    bool CreateD3D12BufferResources(UINT index, D3D12_CPU_DESCRIPTOR_HANDLE& rtvHandle);
-    bool InitializeGUI();
-    void ConfigureSwapChainAndWindow(IDXGISwapChain* swapChain);
+    std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, MAX_BUFFERS> d3d12RenderTargets;
+    std::array<Microsoft::WRL::ComPtr<ID3D11Resource>, MAX_BUFFERS> d3d11WrappedBackBuffers;
+    std::array<Microsoft::WRL::ComPtr<ID3D11RenderTargetView>, MAX_BUFFERS> d3d11RenderTargetViews;
+    
+    UINT64 fenceValue = 0;
+    HANDLE fenceEvent = nullptr;
+    void (*commandQueueCallback)() = nullptr;
 
     void RenderFrame();
-    bool PrepareD3D12Resources();
-    void FinalizeD3D12Rendering();
-    void UpdateViewportIfNeeded();
+    bool InitD3DResources(IDXGISwapChain* swapChain);
+    bool InitD3D11();
+    bool InitD3D12();
+    void UpdateViewport();
+    bool SignalAndWait();
+    void ReleaseResources();
     
-    bool WaitForD3D12CommandQueue();
-    bool SignalFenceAndWait(UINT64 fenceValueToSignal = 0);
-    void ReleaseViewsBuffersAndContext();
-    void CleanupD3D12();
-
-    inline bool CheckSuccess(HRESULT hr) {
-        if (FAILED(hr)) {
-            logger.Log("DirectX operation failed");
-            return false;
+    template<typename T>
+    constexpr bool IsValid(T handle) const noexcept {
+        if constexpr (std::is_same_v<T, HANDLE>) {
+            return handle && handle != INVALID_HANDLE_VALUE;
+        } else {
+            return handle != nullptr;
         }
-        return true;
     }
 };
