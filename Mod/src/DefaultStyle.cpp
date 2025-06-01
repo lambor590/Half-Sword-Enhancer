12e5bb9021e07b2a1e2e09bb7405fae6ee437af7
lambor590 <lambor590@gmail.com>
2025-06-01 16:16:50 +0200

    refactor: optimize DefaultStyle color application
    - Replaced individual color assignments with precomputed color array
    - Streamlined ApplyGlobalStyle method for better performance

--- Mod/src/DefaultStyle.cpp
+++ Mod/src/DefaultStyle.cpp
@@ -2,56 +2,12 @@
 
 void DefaultStyle::ApplyGlobalStyle() {
     ImGuiStyle& style = ImGui::GetStyle();
-    ImVec4* colors = style.Colors;
     
-    colors[ImGuiCol_WindowBg] = black;
-    colors[ImGuiCol_Border] = oldBrass;
-    colors[ImGuiCol_BorderShadow] = shadow;
-    colors[ImGuiCol_Text] = parchment;
-    colors[ImGuiCol_TextDisabled] = ImVec4(0.65f, 0.60f, 0.55f, 1.00f);
+    static constexpr auto precomputedColors = GetColorArray();
     
-    colors[ImGuiCol_Header] = ImVec4(0.28f, 0.19f, 0.11f, 1.00f);
-    colors[ImGuiCol_HeaderHovered] = ImVec4(0.38f, 0.26f, 0.15f, 1.00f);
-    colors[ImGuiCol_HeaderActive] = ImVec4(0.44f, 0.30f, 0.18f, 1.00f);
-    
-    colors[ImGuiCol_Tab] = darkWood;
-    colors[ImGuiCol_TabHovered] = mediumWood;
-    colors[ImGuiCol_TabActive] = lightWood;
-    colors[ImGuiCol_TabUnfocused] = darkInk;
-    colors[ImGuiCol_TabUnfocusedActive] = darkWood;
-    
-    colors[ImGuiCol_Button] = darkWood;
-    colors[ImGuiCol_ButtonHovered] = lightWood;
-    colors[ImGuiCol_ButtonActive] = oldBrass;
-    
-    colors[ImGuiCol_FrameBg] = darkWood;
-    colors[ImGuiCol_FrameBgHovered] = mediumWood;
-    colors[ImGuiCol_FrameBgActive] = lightWood;
-    
-    colors[ImGuiCol_CheckMark] = brightBrass;
-    
-    colors[ImGuiCol_ScrollbarBg] = darkInk;
-    colors[ImGuiCol_ScrollbarGrab] = darkLeather;
-    colors[ImGuiCol_ScrollbarGrabHovered] = mediumWood;
-    colors[ImGuiCol_ScrollbarGrabActive] = oldBrass;
-    
-    colors[ImGuiCol_TitleBg] = darkWood;
-    colors[ImGuiCol_TitleBgActive] = mediumWood;
-    colors[ImGuiCol_TitleBgCollapsed] = darkInk;
-    
-    colors[ImGuiCol_SliderGrab] = darkLeather;
-    colors[ImGuiCol_SliderGrabActive] = oldBrass;
-    
-    colors[ImGuiCol_ResizeGrip] = darkLeather;
-    colors[ImGuiCol_ResizeGripHovered] = mediumWood;
-    colors[ImGuiCol_ResizeGripActive] = oldBrass;
-    
-    colors[ImGuiCol_Separator] = oldBrass;
-    colors[ImGuiCol_SeparatorHovered] = mediumWood;
-    colors[ImGuiCol_SeparatorActive] = brightBrass;
-    
-    colors[ImGuiCol_TextSelectedBg] = ImVec4(0.70f, 0.55f, 0.30f, 0.35f);
-    colors[ImGuiCol_PopupBg] = ImVec4(0.12f, 0.09f, 0.06f, 0.98f);
+    for (int i = 0; i < ImGuiCol_COUNT; ++i) {
+        style.Colors[i] = precomputedColors[i];
+    }
     
     style.WindowPadding = ImVec2(18, 18);
     style.FramePadding = ImVec2(10, 8);
