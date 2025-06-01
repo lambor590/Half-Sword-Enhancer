2ff538bed7e63f42b1d491e82807ec6b4d45d95a
lambor590 <lambor590@gmail.com>
2025-06-01 14:35:11 +0200

    refactor: add noexcept specifiers and default destructor to ID3DRenderer
    - Updated function signatures to include noexcept for safety
    - Added default destructor for proper resource management
    - Improved code clarity and consistency

--- Mod/include/Render/ID3DRenderer.h
+++ Mod/include/Render/ID3DRenderer.h
@@ -1,16 +1,19 @@
 #pragma once
 
 #include <d3d11.h>
+#include <dxgi1_4.h>
 
 #include "IRenderCallback.h"
 
 class ID3DRenderer
 {
 public:
-    virtual void OnPresent(IDXGISwapChain* pThis, UINT syncInterval, UINT flags) = 0;
-    virtual void OnResizeBuffers(IDXGISwapChain* pThis, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags) {};
-    virtual void AddRenderCallback(IRenderCallback* object) {};
-    virtual void SetCommandQueue(ID3D12CommandQueue* commandQueue) {};
-    virtual void SetGetCommandQueueCallback(void (*callback)()) {};
-    virtual void Cleanup() {};
+    virtual void OnPresent(IDXGISwapChain* pThis, UINT syncInterval, UINT flags) noexcept = 0;
+    virtual void OnResizeBuffers(IDXGISwapChain* pThis, UINT bufferCount, UINT width, UINT height, DXGI_FORMAT newFormat, UINT swapChainFlags) noexcept {};
+    virtual void AddRenderCallback(IRenderCallback* object) noexcept {};
+    virtual void SetCommandQueue(ID3D12CommandQueue* commandQueue) noexcept {};
+    virtual void SetGetCommandQueueCallback(void (*callback)()) noexcept {};
+    virtual void Cleanup() noexcept {};
+    
+    virtual ~ID3DRenderer() = default;
 };
