6fb0ea0679206b0e2ddbc5b6990aa03a20d0043f
lambor590 <lambor590@gmail.com>
2025-06-01 16:27:27 +0200

    refactor: simplify CollapsibleSection rendering logic
    - Replaced manual style management with StyleRAII for cleaner code
    - Streamlined Render method by removing unnecessary style pushes and pops
    - Enhanced readability of the collapsible section rendering

--- Mod/src/CollapsibleSection.cpp
+++ Mod/src/CollapsibleSection.cpp
@@ -8,25 +8,13 @@ void CollapsibleSection::Render() {
     ImGui::SetNextItemOpen(true, ImGuiCond_Once);
     bool isOpen = ImGui::CollapsingHeader(name.c_str());
     
-    if (isOpen) {
-        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(8, 6));
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(10, 8));
-        ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, 25.0f);
-        
-        ImGui::Indent(10.0f);
-        ImGui::Spacing();
+    if (isOpen) [[likely]] {
+        SectionStyle::StyleRAII style;
         
         const size_t count = functions.size();
         for (size_t i = 0; i < count; ++i) {
             functions[i]->Render();
             if (i < count - 1) ImGui::Spacing();
         }
-        
-        ImGui::Unindent(10.0f);
-        ImGui::PopStyleVar(3);
     }
-}
-
-void CollapsibleSection::AddFunction(std::unique_ptr<IMenuFunction> function) {
-    functions.push_back(std::move(function));
 }
