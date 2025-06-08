9755d2ed654ed676221fb545e4c3788abca95e52
lambor590 <lambor590@gmail.com>
2025-06-08 23:55:04 +0200

    feat: integrate NotificationManager into GUI
    - Added NotificationManager header inclusion
    - Updated WndProc to handle mouse capture correctly
    - Initialized and updated NotificationManager in Setup and Render methods

--- Mod/src/Gui.cpp
+++ Mod/src/Gui.cpp
@@ -1,5 +1,6 @@
 #include "Gui.h"
 #include "KeybindManager.h"
+#include "NotificationManager.h"
 
 WNDPROC Gui::originalWndProc = nullptr;
 bool Gui::isVisible = true;
@@ -15,18 +16,12 @@ LRESULT CALLBACK Gui::WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
         return true;
     }
 
-    if (isVisible && (
-        ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam) || 
-        (msg >= WM_MOUSEFIRST && msg <= WM_MOUSELAST && ImGui::GetIO().WantCaptureMouse)
-    )) {
+    if (isVisible && (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam) || 
+        (msg >= WM_MOUSEFIRST && msg <= WM_MOUSELAST))) {
         return true;
     }
 
     return CallWindowProc(originalWndProc, hWnd, msg, wParam, lParam);
-}
-
-void Gui::SetupStyle() {
-    DefaultStyle::ApplyGlobalStyle();
 }
 
 void Gui::Setup() {    
@@ -40,10 +35,12 @@ void Gui::Setup() {
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_NavEnableGamepad;
     io.IniFilename = _strdup((ConfigManager::GetAppDataPath() / "imgui.ini").string().c_str());
 
-    SetupStyle();
+    DefaultStyle::ApplyGlobalStyle();
 
     ImGui::SetNextWindowSize(ImVec2(699, 389), ImGuiCond_Once);
     ImGui::SetNextWindowPos(ImVec2(544, 331), ImGuiCond_Once, ImVec2(0.5f, 0.5f));
+
+    NotificationManager::Initialize();
 
     MenuManager::Get().AddSection<PlayerSection>(MenuTab::Gameplay);
     MenuManager::Get().AddSection<WorldSection>(MenuTab::Gameplay);
@@ -57,11 +54,23 @@ void Gui::Render() {
     ImGui_ImplDX11_NewFrame();
     ImGui::NewFrame();
 
-    ImGui::Begin("Half Sword Enhancer", &isVisible, ImGuiWindowFlags_NoCollapse);
+    if (!isVisible) {
+        ImGuiIO& io = ImGui::GetIO();
+        io.WantCaptureMouse = false;
+        io.WantCaptureKeyboard = false;
+        io.WantTextInput = false;
+    }
 
-    MenuManager::Get().RenderMenu();
+    NotificationManager::Update();
 
-    ImGui::End();
+    if (isVisible) {
+        ImGui::Begin("Half Sword Enhancer", &isVisible, ImGuiWindowFlags_NoCollapse);
+        MenuManager::Get().RenderMenu();
+        ImGui::End();
+    }
+
+    NotificationManager::Render();
+
     ImGui::Render();
     ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 }
