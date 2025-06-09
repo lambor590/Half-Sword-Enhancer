5b317bd645cedad6b134ccb3b7cd7fe9321e23a9
lambor590 <lambor590@gmail.com>
2025-06-09 18:47:19 +0200

    refactor: implement FNV hash function for string processing
    - Added hash_string function for consistent string hashing
    - Updated OnProcessEvent to use new hash function
    - Enhanced RegisterHook and UnregisterHook methods with classHash

--- Mod/src/GameHook.cpp
+++ Mod/src/GameHook.cpp
@@ -3,15 +3,28 @@
 
 static GameHook* hookInstance = &GameHook::Get();
 
+static inline uint64_t hash_string(std::string_view str) noexcept {
+    constexpr uint64_t FNV_OFFSET_BASIS = 14695981039346656037ULL;
+    constexpr uint64_t FNV_PRIME = 1099511628211ULL;
+    
+    uint64_t hash = FNV_OFFSET_BASIS;
+    for (char c : str) {
+        hash ^= static_cast<uint64_t>(static_cast<unsigned char>(c));
+        hash *= FNV_PRIME;
+    }
+    return hash;
+}
+
 inline static void* __stdcall OnProcessEvent(SDK::UObject* pObject, SDK::UFunction* pFunc, void* Parms)
 {
     const std::string& funcName = pFunc->GetName();
-    uint64_t hash = fnv1a_hash(funcName);
-
-    if (auto it = hookInstance->hookMap.find(hash); it != hookInstance->hookMap.end()) {
-        const auto& hd = it->second;
-        if (hd.funcName == funcName && (hd.className.empty() || pObject->Class->GetName() == hd.className)) {
-            hd.callback();
+    uint64_t funcHash = hash_string(funcName);
+        
+    if (auto it = hookInstance->hookMap.find(funcHash); it != hookInstance->hookMap.end()) {
+        const auto& hookData = it->second;
+        if (hookData.className.empty() ||
+            hash_string(pObject->Class->GetName()) == hookData.classHash) {
+            hookData.callback();
         }
     }
 
@@ -46,4 +59,18 @@ void GameHook::Unhook() const
 {
     MemoryUtils::Unhook(oProcessEvent);
     logger.Log("ProcessEvent unhooked successfully!");
+}
+
+void GameHook::RegisterHook(const std::string& functionName, std::function<void()> callback) {
+    auto [hookClass, hookFunc] = ParseFunctionName(functionName);
+    uint64_t funcHash = hash_string(hookFunc);
+    uint64_t classHash = hookClass.empty() ? 0 : hash_string(hookClass);
+    
+    hookMap[funcHash] = HookData(std::string(hookClass), std::string(hookFunc), classHash, std::move(callback));
+}
+
+void GameHook::UnregisterHook(const std::string& functionName) {
+    auto [_, hookFunc] = ParseFunctionName(functionName);
+    uint64_t hash = hash_string(hookFunc);
+    hookMap.erase(hash);
 }
