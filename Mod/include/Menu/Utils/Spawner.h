b5d4a18e8f6621e6b6978769331760383ce4c0a4
lambor590 <lambor590@gmail.com>
2025-07-31 19:54:57 +0200

    feat: add snap to ground parameter to SpawnActor function
    - Updated SpawnActor to include snapToGround parameter
    - Added snapToGround field to SpawnRequest struct

--- Mod/include/Menu/Utils/Spawner.h
+++ Mod/include/Menu/Utils/Spawner.h
@@ -13,8 +13,10 @@ namespace Spawner {
         std::string classPath;
         SDK::FTransform transform;
         std::function<void(SDK::AActor*)> callback;
+        bool snapToGround;
     };
 
-    void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback = nullptr);
+    SDK::FVector GetGroundPosition(const SDK::UWorld* world, SDK::FVector position, float groundOffset = 50.0f, float traceDistance = 1000.0f);
+    void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback = nullptr, bool snapToGround = false);
     void ProcessSpawnQueue();
 }
