03c9aa40553ca8c4822792b614c05bd6cb759c83
lambor590 <lambor590@gmail.com>
2025-07-31 15:58:40 +0200

    feat: add CommonHeaders for utility includes

---
+++ Mod/include/Utils/CommonHeaders.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <unordered_map>
+#include <chrono>
+#include <filesystem>
+#include <array>
+#include <algorithm>
+#include <thread>
+#include <string_view>
+#include <initializer_list>
+
+#include <Windows.h>
+
+#include "SDK/Basic.hpp"
+#include "SDK/CoreUObject_classes.hpp"
+#include "SDK/Engine_classes.hpp"
+#include "SDK/Willie_BP_classes.hpp"
