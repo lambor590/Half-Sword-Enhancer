c4a45ae51dc7816a0cbf011d864e2f5d59e524f0
lambor590 <lambor590@gmail.com>
2025-07-31 20:05:04 +0200

    feat: enhance ground positioning logic for spawned actors
    - Added GetGroundOffset function to determine offset based on class type
    - Implemented GetGroundPosition function for accurate ground placement
    - Updated SpawnActor to include snapToGround parameter in spawn requests

--- Mod/src/Spawner.cpp
+++ Mod/src/Spawner.cpp
@@ -1,11 +1,36 @@
 #include <queue>
 #include <mutex>
+#include <iostream>
 
 #include "Menu/Utils/Spawner.h"
+#include "SDK/CoreUObject_classes.hpp"
+#include "SDK/Engine_classes.hpp"
 
 namespace Spawner {
     static std::queue<SpawnRequest> spawnQueue;
     static std::mutex queueMutex;
+
+    static float GetGroundOffset(const std::string& classPath, const SDK::FVector& actorScale) {
+        float baseOffset = 50.0f;
+        
+        if (classPath.find("Willie_BP") != std::string::npos) {
+            baseOffset = 100.0f;
+        }
+        else if (classPath.find("ModularWeapon") != std::string::npos || 
+                 classPath.find("Shield") != std::string::npos) {
+            baseOffset = 30.0f;
+        }
+        else if (classPath.find("Dagger") != std::string::npos || 
+                 classPath.find("Tool") != std::string::npos) {
+            baseOffset = 15.0f;
+        }
+        else if (classPath.find("Armor") != std::string::npos) {
+            baseOffset = 25.0f;
+        }
+        
+        float scaledOffset = baseOffset * (static_cast<float>(actorScale.Z) * 1.2f);
+        return scaledOffset;
+    }
 
     static SDK::AActor* SpawnActorInternal(const SDK::UWorld* world, const std::string& classPath, const SDK::FTransform& transform) {
         std::wstring wClassName(classPath.begin(), classPath.end());
@@ -25,9 +50,44 @@ namespace Spawner {
         return actor;
     }
 
-    void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback) {
+    SDK::FVector GetGroundPosition(const SDK::UWorld* world, SDK::FVector position, float groundOffset, float traceDistance) {
+        SDK::FHitResult hitResult;
+        SDK::FVector startPos = position;
+        startPos.Z += 1000.0f;
+        SDK::FVector endPos = position;
+        endPos.Z -= 1000.0f;
+        
+        SDK::TArray<SDK::AActor*> actorsToIgnore;
+        
+        bool hit = SDK::UKismetSystemLibrary::LineTraceSingle(
+            world,
+            startPos,
+            endPos,
+            SDK::ETraceTypeQuery::TraceTypeQuery1,
+            true,
+            actorsToIgnore,
+            SDK::EDrawDebugTrace::ForOneFrame,
+            &hitResult,
+            true,
+            SDK::FLinearColor(1.0f, 0.0f, 0.0f, 1.0f),
+            SDK::FLinearColor(0.0f, 1.0f, 0.0f, 1.0f),
+            5.0f
+        );
+        
+        if (hit && hitResult.bBlockingHit) {
+            SDK::FVector groundPos = hitResult.Location;
+            groundPos.Z += groundOffset;
+            return groundPos;
+        }
+        
+        SDK::FVector groundLevel = position;
+        groundLevel.Z = groundOffset;
+        return groundLevel;
+    }
+
+    void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback, bool snapToGround) {
         std::lock_guard<std::mutex> lock(queueMutex);
-        spawnQueue.push({world, className, transform, callback});
+        spawnQueue.push({world, className, transform, callback, snapToGround});
     }
 
     void ProcessSpawnQueue() {
@@ -36,7 +96,15 @@ namespace Spawner {
         while (!spawnQueue.empty()) {
             const auto& request = spawnQueue.front();
             
-            SDK::AActor* spawnedActor = SpawnActorInternal(request.world, request.classPath, request.transform);
+            SDK::FTransform finalTransform = request.transform;
+            
+            if (request.snapToGround) {
+                float groundOffset = GetGroundOffset(request.classPath, request.transform.Scale3D);
+                SDK::FVector groundPosition = GetGroundPosition(request.world, request.transform.Translation, groundOffset);
+                finalTransform.Translation = groundPosition;
+            }
+            
+            SDK::AActor* spawnedActor = SpawnActorInternal(request.world, request.classPath, finalTransform);
             
             if (request.callback) {
                 request.callback(spawnedActor);
