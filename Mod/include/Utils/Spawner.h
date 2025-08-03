b920d6c44e2fff1ab5a7422c23fe87523f9efae7
lambor590 <lambor590@gmail.com>
2025-08-04 01:19:43 +0200

    refactor: replace QueueSpawnActor with SpawnActor method

--- Mod/include/Utils/Spawner.h
+++ Mod/include/Utils/Spawner.h
@@ -29,6 +29,5 @@ namespace Spawner {
     float GetGroundOffsetForType(ActorType type, const SDK::FVector& scale = {1.0f, 1.0f, 1.0f});
     
     SDK::FVector GetGroundPosition(const SDK::UWorld* world, SDK::FVector position, float groundOffset = 50.0f, float traceDistance = 1000.0f);
-    void QueueSpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback = nullptr, bool snapToGround = false);
-    void ProcessSpawnQueue();
+    void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback = nullptr, bool snapToGround = false);
 }
