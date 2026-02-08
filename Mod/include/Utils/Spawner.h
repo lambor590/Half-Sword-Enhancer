2ee3fc924b3efe897e1f76778d334c957294756e
lambor590 <lambor590@gmail.com>
2026-02-08 22:46:55 +0100

    feat: improve weapon spawning with modular assembly and class caching
    Weapons now spawn fully assembled with correct parts (grip, head, guard,
    pommel) by reading the Class Default Object and building a passport.
    Added UClass cache to avoid redundant LoadClassAsset calls. Ground trace
    now uses LineTraceSingleForObjects for more reliable placement.

--- Mod/include/Utils/Spawner.h
+++ Mod/include/Utils/Spawner.h
@@ -17,7 +17,8 @@ namespace Spawner {
 
     ActorType GetActorType(const std::string& classPath);
     float GetGroundOffsetForType(ActorType type, const SDK::FVector& scale = {1.0f, 1.0f, 1.0f});
-    
+    void ClearCache();
+
     SDK::FVector GetGroundPosition(const SDK::UWorld* world, SDK::FVector position, float groundOffset = 50.0f, float traceDistance = 1000.0f);
     void SpawnActor(const SDK::UWorld* world, const std::string& className, const SDK::FTransform& transform, std::function<void(SDK::AActor*)> callback = nullptr, bool snapToGround = false);
 }
