4d2477885b950bb9d52286fbe374d97045b30d24
lambor590 <lambor590@gmail.com>
2026-02-08 03:27:51 +0100

    feat: add currentModularArmorSubcategoryIndex to PlayerConfig
    - Introduced a new variable to track the modular armor subcategory index

--- Mod/include/Menu/SectionConfig.h
+++ Mod/include/Menu/SectionConfig.h
@@ -75,6 +75,7 @@ namespace SectionConfig {
 
         uint8_t currentCategoryIndex = 0;
         uint8_t currentWeaponSubcategoryIndex = 0;
+        uint8_t currentModularArmorSubcategoryIndex = 0;
         uint16_t currentItemIndex = 0;
     };
 
