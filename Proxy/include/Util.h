4f1ee2930e289c69ce12e6f6004efab45640fe06
lambor590 <lambor590@gmail.com>
2026-02-23 12:32:20 +0100

    chore: clean build system and remove dead proxy code
    - Remove PROXY_EXPORTS from Proxy.vcxproj (never referenced)
    - Remove duplicate OptimizeReferences from Launcher.vcxproj
    - Remove useless mutex in Proxy LoadModDLL() (created but never waited on)
    - Add *.aps to .gitignore

--- Proxy/include/Util.h
+++ Proxy/include/Util.h
@@ -13,16 +13,13 @@ static HMODULE LoadOriginalDLL()
 
 static bool LoadModDLL()
 {
-    HANDLE hMutex = CreateMutexA(NULL, FALSE, "HSEnhancerProxyInit");
     HMODULE hModDLL = LoadLibraryA("HSEnhancer.dll");
     if (hModDLL) {
         typedef void(*InitFn)();
         InitFn init = (InitFn)GetProcAddress(hModDLL, "HSE_Initialize");
         if (init) init();
-        if (hMutex) CloseHandle(hMutex);
         return true;
     }
-    if (hMutex) CloseHandle(hMutex);
     MessageBoxA(NULL,
         "Could not find 'HSEnhancer.dll'."
         "\n\nPlease make sure the file is named 'HSEnhancer.dll' and is in the same folder as the game.",
