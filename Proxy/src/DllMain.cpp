cb1c0e65cd24907ae67263024be7eb161033585b
lambor590 <lambor590@gmail.com>
2026-03-21 00:13:19 +0100

    style: remove unused parameters in DllMain function

--- Proxy/src/DllMain.cpp
+++ Proxy/src/DllMain.cpp
@@ -64,7 +64,7 @@ static void CacheOriginalFunctions()
         originalFuncs[i] = GetProcAddress(hOriginalDLL, kFuncNames[i]);
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD ul_reason_for_call, LPVOID /*lpReserved*/)
 {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         hOriginalDLL = LoadOriginalDLL();
