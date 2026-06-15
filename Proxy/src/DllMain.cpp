46d7325393aec4bde65dff05a3ac2d7ff78640a3
lambor590 <lambor590@gmail.com>
2026-06-15 17:38:15 +0200

    refactor(proxy): inline dll loading

--- Proxy/src/DllMain.cpp
+++ Proxy/src/DllMain.cpp
@@ -1,12 +1,38 @@
 #include "Windows.h"
 
-#include "Util.h"
-
 static constexpr int FUNC_COUNT = 180;
 
 extern "C" FARPROC originalFuncs[FUNC_COUNT]{};
 
 static HMODULE hOriginalDLL = NULL;
+
+namespace {
+    HMODULE LoadOriginalDLL() {
+        char systemPath[MAX_PATH];
+        GetSystemDirectoryA(systemPath, MAX_PATH);
+        char originalDllPath[MAX_PATH];
+        wsprintfA(originalDllPath, "%s\\winmm.dll", systemPath);
+        return LoadLibraryA(originalDllPath);
+    }
+
+    bool LoadModDLL() {
+        HMODULE hModDLL = LoadLibraryA("HSEnhancer.dll");
+        if (hModDLL) {
+            using InitFn = void (*)();
+            auto init = reinterpret_cast<InitFn>(GetProcAddress(hModDLL, "HSE_Initialize"));
+            if (init) init();
+            return true;
+        }
+
+        MessageBoxA(
+            NULL,
+            "Could not find 'HSEnhancer.dll'."
+            "\n\nPlease make sure the file is named 'HSEnhancer.dll' and is in the same folder as the game.",
+            "Half Sword Enhancer", MB_OK | MB_ICONINFORMATION
+        );
+        return false;
+    }
+}
 
 static DWORD WINAPI BootstrapMod(LPVOID) {
     LoadModDLL();
