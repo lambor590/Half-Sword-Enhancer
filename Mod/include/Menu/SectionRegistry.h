54beaa1fcbe5d8a48773e07fd4cbf5959cf5c588
lambor590 <lambor590@gmail.com>
2026-06-15 18:00:45 +0200

    refactor(mod): inline section registration

--- Mod/include/Menu/SectionRegistry.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-class MenuManager;
-class ModContext;
-
-void RegisterModSections(MenuManager& menu, ModContext& ctx);
--- Mod/src/Gui.cpp
+++ Mod/src/Gui.cpp
@@ -3,7 +3,21 @@
 #include "Core/ModContext.h"
 #include "DefaultStyle.h"
 #include "Menu/MenuManager.h"
-#include "Menu/SectionRegistry.h"
+#include "Menu/Sections/Equipment/ArmorEditorSection.h"
+#include "Menu/Sections/Equipment/LoadoutManagerSection.h"
+#include "Menu/Sections/Equipment/WeaponEditorSection.h"
+#include "Menu/Sections/Player/PlayerAbilitiesSection.h"
+#include "Menu/Sections/Player/PlayerEditorSection.h"
+#include "Menu/Sections/Settings/AssetOverridesSection.h"
+#include "Menu/Sections/Settings/GraphicsSection.h"
+#include "Menu/Sections/Settings/GuiSection.h"
+#include "Menu/Sections/Spawner/ItemSpawnerSection.h"
+#include "Menu/Sections/Spawner/NPCEditorSection.h"
+#include "Menu/Sections/World/AIDirectorSection.h"
+#include "Menu/Sections/World/MapLoaderSection.h"
+#include "Menu/Sections/World/SkyEditorSection.h"
+#include "Menu/Sections/World/WorldActionsSection.h"
+#include "Menu/Sections/World/WorldEditorSection.h"
 #include "KeybindManager.h"
 #include "Logger.h"
 #include "NotificationManager.h"
@@ -114,7 +128,21 @@ void Gui::Setup() {
 
     auto& ctx = ModContext::Get();
     auto& menu = MenuManager::Get();
-    RegisterModSections(menu, ctx);
+    menu.AddSection<GuiSection>(ctx);
+    menu.AddSection<AssetOverridesSection>(ctx);
+    menu.AddSection<GraphicsSection>(ctx);
+    menu.AddSection<AIDirectorSection>(ctx);
+    menu.AddSection<WorldActionsSection>(ctx);
+    menu.AddSection<WorldEditorSection>(ctx);
+    menu.AddSection<SkyEditorSection>(ctx);
+    menu.AddSection<MapLoaderSection>(ctx);
+    menu.AddSection<PlayerAbilitiesSection>(ctx);
+    menu.AddSection<PlayerEditorSection>(ctx);
+    menu.AddSection<NPCEditorSection>(ctx);
+    menu.AddSection<ItemSpawnerSection>(ctx);
+    menu.AddSection<ArmorEditorSection>(ctx);
+    menu.AddSection<WeaponEditorSection>(ctx);
+    menu.AddSection<LoadoutManagerSection>(ctx);
 
     originalWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
     logger.Log("WndProc hooked successfully");
